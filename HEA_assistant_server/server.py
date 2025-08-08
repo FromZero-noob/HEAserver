ea10ef32ff8840ab9b7fa1265853efd08ab6d5ed
Junshang Zhang <anton_616@qq.com>
2025-08-08 14:23:49 +0800

    Update server.py

--- HEA_assistant_server/server.py
+++ HEA_assistant_server/server.py
@@ -221,4 +221,5 @@ def HEA_predictor(name:List[str])->List[str]:
     return(res)
 
 if __name__ == "__main__":
-    mcp.run(transport="sse")
+    mcp.run(transport="sse",host="0.0.0.0",port=50002)
+
